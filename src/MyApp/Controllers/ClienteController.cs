d77237b6970b7e4c72e23eb72c5b96743810316e
cesar augusto gadelha <cesar_ags@outlook.com>
2024-01-24 18:12:21 -0300

    adicionado endpoint cliente

---
+++ src/MyApp/Controllers/ClienteController.cs
@@ -0,0 +1,16 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace MyApp.Controllers
+{
+    [ApiController]
+    [Route("[controller]")]
+    public class ClienteController : ControllerBase
+    {
+
+        [HttpGet(Name = "Clientes")]
+        public async Task<IActionResult> Index()
+        {
+            return Ok("Bem vindo");
+        }
+    }
+}
